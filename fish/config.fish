56518c2c637fd040ea4581e3a132099dc5abb339
Jan Lafferton <jan@lafferton.de>
2023-10-06 14:02:45 +0200

    Initial commit

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    fish_hybrid_key_bindings
+    if test -z "$CONTAINER_ID"
+        starship init fish | source
+    end
+end
+
+set -Ux EDITOR nvim
+
+fish_add_path ~/.cargo/bin
+fish_add_path ~/Scripts
