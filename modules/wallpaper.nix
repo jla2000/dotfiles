dccdb18ec20fb1efe7d76d69de390c41b138bf60
Jan Lafferton <jan@lafferton.de>
2023-12-16 14:40:47 +0100

    Fix

--- modules/wallpaper.nix
+++ modules/wallpaper.nix
@@ -1,29 +1,28 @@
-# { config, pkgs, lib, ... }:
-# let
-#   change-wallpaper = pkgs.writeShellApplication {
-#     name = "change-wallpaper.sh";
-#     text = ''
-#       swww img ${config.wallpaper};
-#     '';
-#   };
-# in
-# {
-#   options = {
-#     wallpaper = {
-#       enable = lib.mkEnableOption { };
-#       path = lib.mkOption {
-#         default = ./wallpapers;
-#         type = lib.types.path;
-#       };
-#     };
-#   };
-#
-#   config = lib.mkIf config.wallpaper.enable {
-#     home.packages = [ pkgs.swww change-wallpaper ];
-#     wayland.windowManager.hyprland.settings.exec-once = [
-#       "swww init"
-#       "${change-wallpaper}/bin/change-wallpaper.sh"
-#     ];
-#   };
-# }
-{ }
+{ config, pkgs, lib, ... }:
+let
+  change-wallpaper = pkgs.writeShellApplication {
+    name = "change-wallpaper.sh";
+    text = ''
+      swww img ${config.wallpaper.path};
+    '';
+  };
+in
+{
+  options = {
+    wallpaper = {
+      enable = lib.mkEnableOption { };
+      path = lib.mkOption {
+        default = ./wallpapers;
+        type = lib.types.path;
+      };
+    };
+  };
+
+  config = lib.mkIf config.wallpaper.enable {
+    home.packages = [ pkgs.swww change-wallpaper ];
+    wayland.windowManager.hyprland.settings.exec-once = [
+      "swww init"
+      "${change-wallpaper}/bin/change-wallpaper.sh"
+    ];
+  };
+}
