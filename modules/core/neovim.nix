0a01a6b89b9574fa08fc798382ac111e6d73f75a
Jan Lafferton <jan@lafferton.de>
2024-02-15 22:09:54 +0100

    Patch neovim

--- modules/core/neovim.nix
+++ modules/core/neovim.nix
@@ -1,16 +1,37 @@
 { pkgs, outputs, ... }:
+let
+  cmakelint = pkgs.writeShellApplication {
+    name = "cmakelint";
+    text = ''
+      ${pkgs.cmake-format}/bin/cmake-lint "$@"
+    '';
+  };
+  patched-nvim = pkgs.writeShellApplication {
+    name = "nvim";
+    runtimeInputs = with pkgs; [
+      stylua
+      lua-language-server
+      nil
+      rnix-lsp
+      marksman
+      markdownlint-cli
+      rust-analyzer
+      libclang
+      vscode-langservers-extracted
+      sqlite
+      neocmakelsp
+      cmake-format
+      cmakelint
+    ];
+    # Custom patch to let sqlite-lua find the sqlite library
+    text = ''
+      ${pkgs.neovim}/bin/nvim --cmd "let g:sqlite_clib_path = '${pkgs.sqlite.out}/lib/libsqlite3.so'" "$@"
+    '';
+  };
+in
 {
   nixpkgs.overlays = [ outputs.overlays.neovim-nightly-overlay ];
-
-  home.packages = with pkgs; [
-    neovim
-    stylua
-    lua-language-server
-    nil
-    rnix-lsp
-    marksman
-    markdownlint-cli
-    rust-analyzer
-    libclang
+  home.packages = [
+    patched-nvim
   ];
 }
