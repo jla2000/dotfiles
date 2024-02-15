e35dc900692b78fc27c46b6c497d58d7a0de9e54
jlafferton <jan.lafferton@vector.com>
2024-02-15 10:26:41 +0100

    Refactor tmux config

--- modules/core/tmux.nix
+++ modules/core/tmux.nix
@@ -17,6 +17,11 @@
       bind-key -T copy-mode-vi v send-keys -X begin-selection
       bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
       bind-key -T copy-mode-vi i send-keys -X cancel
+
+      set -g status-style bg=default
+      set -g status-left-length 90
+      set -g status-right-length 90
+      set -g status-justify absolute-centre
     '';
     plugins = with pkgs.tmuxPlugins; [
       vim-tmux-navigator
@@ -25,19 +30,15 @@
         extraConfig = /* tmux */ ''
           set -g @catppuccin_flavour 'macchiato'
 
-          set -g @catppuccin_window_right_separator "█ "
+          set -g @catppuccin_window_left_separator ""
+          set -g @catppuccin_window_right_separator " "
           set -g @catppuccin_window_number_position "right"
           set -g @catppuccin_window_middle_separator " | "
 
           set -g @catppuccin_window_default_fill "none"
           set -g @catppuccin_window_current_fill "all"
 
-          set -g @catppuccin_status_modules "cpu date_time"
-          set -g @catppuccin_status_left_separator "█"
-          set -g @catppuccin_status_right_separator "█"
-          set -g @catppuccin_status_fill "all"
-
-          set -g @catppuccin_date_time_text "%Y-%m-%d"
+          set -g @catppuccin_status_modules "cpu"
         '';
       }
       cpu
