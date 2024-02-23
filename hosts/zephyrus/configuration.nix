60eb1d65a834afbc3c12abf6b924bb614fb0236c
Jan Lafferton <jan@lafferton.de>
2024-02-23 16:36:31 +0100

    update config

--- hosts/zephyrus/configuration.nix
+++ hosts/zephyrus/configuration.nix
@@ -53,11 +53,28 @@
   services.blueman.enable = true;
 
   # Configure xserver settings
-  services.xserver.enable = true;
+  environment.pathsToLink = [ "/libexec" ];
 
   # Configure login manager and window manager
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.defaultSession = "hyprland";
+  services.xserver = {
+    enable = true;
+    desktopManager.xterm.enable = false;
+
+    displayManager = {
+      defaultSession = "none+i3";
+      gdm.enable = true;
+    };
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu #application launcher most people use
+        i3status # gives you the default i3 status bar
+        i3lock #default i3 screen locker
+        i3blocks #if you are planning on using i3blocks over i3status
+     ];
+    };
+  };
 
   programs.hyprland.enable = true;
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
--- modules/apps/firefox.nix
+++ modules/apps/firefox.nix
@@ -2,22 +2,12 @@
 {
   programs.firefox = {
     enable = true;
-    package = pkgs.firefox-wayland;
     profiles = {
       default = {
-        settings = {
-          "layout.css.devPixelsPerPx" = "2.3";
-        };
         extensions = with pkgs.nur.repos.rycee.firefox-addons; [
           ublock-origin
           vimium
           firenvim
-        ];
-        bookmarks = [
-          {
-            name = "Home-Manager Options";
-            url = "https://nix-community.github.io/home-manager/options.html";
-          }
         ];
       };
     };
