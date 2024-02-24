7a5994b5b5381bc7819b8a55c84daa69ab3b4c3d
Jan Lafferton <jan@lafferton.de>
2024-02-24 09:35:53 +0100

    Make firefox slightly bigger

--- modules/apps/firefox.nix
+++ modules/apps/firefox.nix
@@ -4,6 +4,9 @@
     enable = true;
     profiles = {
       default = {
+        settings = {
+          "layout.css.devPixelsPerPx" = "1.25";
+        };
         extensions = with pkgs.nur.repos.rycee.firefox-addons; [
           ublock-origin
           vimium
