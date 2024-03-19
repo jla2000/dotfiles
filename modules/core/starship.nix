9cd895c3460ea9c88f434c6f5bb5c54f16d1c19c
Jan Lafferton <jan@lafferton.de>
2024-03-19 18:07:10 +0100

    Update inputs + Use old prompt

--- modules/core/starship.nix
+++ modules/core/starship.nix
@@ -2,17 +2,6 @@
   programs.starship = {
     enable = true;
     settings = {
-      character = {
-        success_symbol = "\\[[INS](bold green)\\] ~>";
-        error_symbol = "\\[[INS](bold green)\\] ~>";
-        vimcmd_symbol = "\\[[NOR](bold blue)\\] ~>";
-        vimcmd_replace_symbol = "\\[[RPL](bold purple)\\] ~>";
-        vimcmd_replace_one_symbol = "\\[[RPL](bold purple)\\] ~>";
-        vimcmd_visual_symbol = "\\[[VIS](bold yellow)\\] ~>";
-      };
-      status = {
-        disabled = false;
-      };
       sudo = {
         disabled = false;
       };
