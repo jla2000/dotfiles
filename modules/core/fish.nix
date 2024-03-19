370ad2eb3a3eda82e804cf9b2b82b534dea54937
jlafferton <jan.lafferton@vector.com>
2024-03-19 12:08:32 +0100

    Improve starship prompt

--- modules/core/fish.nix
+++ modules/core/fish.nix
@@ -9,11 +9,17 @@
     settings = {
       character = {
         success_symbol = "\\[[INS](bold green)\\] ~>";
-        error_symbol = "\\[[INS](bold red)\\] ~>";
+        error_symbol = "\\[[INS](bold green)\\] ~>";
         vimcmd_symbol = "\\[[NOR](bold blue)\\] ~>";
         vimcmd_replace_symbol = "\\[[RPL](bold purple)\\] ~>";
         vimcmd_replace_one_symbol = "\\[[RPL](bold purple)\\] ~>";
         vimcmd_visual_symbol = "\\[[VIS](bold yellow)\\] ~>";
+      };
+      status = {
+        disabled = false;
+      };
+      sudo = {
+        disabled = false;
       };
     };
   };
