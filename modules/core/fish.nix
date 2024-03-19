2f32d74a4fb6fc21a5c9f910b888740c517d60cb
jlafferton <jan.lafferton@vector.com>
2024-03-19 12:12:53 +0100

    Missing stuff

--- modules/core/fish.nix
+++ modules/core/fish.nix
@@ -4,26 +4,6 @@
     zoxide
   ];
 
-  programs.starship = {
-    enable = true;
-    settings = {
-      character = {
-        success_symbol = "\\[[INS](bold green)\\] ~>";
-        error_symbol = "\\[[INS](bold green)\\] ~>";
-        vimcmd_symbol = "\\[[NOR](bold blue)\\] ~>";
-        vimcmd_replace_symbol = "\\[[RPL](bold purple)\\] ~>";
-        vimcmd_replace_one_symbol = "\\[[RPL](bold purple)\\] ~>";
-        vimcmd_visual_symbol = "\\[[VIS](bold yellow)\\] ~>";
-      };
-      status = {
-        disabled = false;
-      };
-      sudo = {
-        disabled = false;
-      };
-    };
-  };
-
   programs.fish = {
     enable = true;
     interactiveShellInit = /* fish */ ''
