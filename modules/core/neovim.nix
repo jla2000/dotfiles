05513c0a731d05629c6d9c366cfb514453574064
Jan Lafferton <jan@lafferton.de>
2024-03-20 13:22:58 +0100

    Add sqlite dependency

--- modules/core/neovim.nix
+++ modules/core/neovim.nix
@@ -86,7 +86,7 @@ let
     nativeBuildInputs = [ pkgs.makeWrapper ];
     postBuild = ''
       wrapProgram $out/bin/nvim \
-        --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [ pkgs.libgit2 ]}" \
+        --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath (with pkgs; [ libgit2 sqlite ])}" \
         --prefix PATH : "${lib.makeBinPath [ runtimeBinaries ] }"
     '';
   };
