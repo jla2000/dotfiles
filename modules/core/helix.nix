0bc8462ce4ab93f279d9ac940e2b8c109b0ef91d
jlafferton <jan.lafferton@vector.com>
2024-04-19 10:37:14 +0200

    Improve helix config

--- modules/core/helix.nix
+++ modules/core/helix.nix
@@ -26,6 +26,7 @@ in
         editor = {
           mouse = true;
           line-number = "relative";
+          cursorline = true;
           true-color = true;
           file-picker.hidden = false;
           lsp.display-messages = true;
@@ -35,15 +36,27 @@ in
           j.k = "normal_mode";
         };
         keys.normal = {
+          # Utility
           C-g = [ ":new" ":insert-output ${pkgs.lazygit}/bin/lazygit" ":buffer-close!" ":redraw" ];
+
+          # Toggle settings
           H = ":toggle lsp.display-inlay-hints";
-          C-h = "jump_view_left";
-          C-l = "jump_view_right";
-          C-j = [ "goto_next_paragraph" ];
-          C-k = [ "goto_prev_paragraph" ];
+
+          # Buffer switching
           tab = "goto_next_buffer";
           S-tab = "goto_previous_buffer";
+
+          # Selection
           X = "extend_line_above";
+
+          # Paragraph movement
+          "}" = "goto_next_paragraph";
+          "{" = "goto_prev_paragraph";
+
+          # Improve g movements
+          g.l = "extend_to_line_end";
+          g.h = "extend_to_line_start";
+          g.s = [ "collapse_selection" "select_mode" "goto_first_nonwhitespace" "exit_select_mode" ];
         };
       };
       languages.language = [
