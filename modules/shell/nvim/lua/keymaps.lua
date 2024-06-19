f6bb51943f08ae762b8f92c6a1e82a0cdd015cd3
Jan Lafferton <jan@lafferton.de>
2024-06-19 20:50:34 +0200

    Small improvements

--- modules/shell/nvim/lua/keymaps.lua
+++ modules/shell/nvim/lua/keymaps.lua
@@ -10,6 +10,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
 		vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
 		vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
-		vim.keymap.set("n", "R", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+		vim.keymap.set("n", "<leader>r", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
 	end,
 })
--- modules/shell/nvim/lua/options.lua
+++ modules/shell/nvim/lua/options.lua
@@ -7,7 +7,7 @@ vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
 vim.o.confirm = true
-vim.o.wrap = true
+vim.o.wrap = false
 vim.o.signcolumn = "number"
 vim.o.scrolloff = 4
 
