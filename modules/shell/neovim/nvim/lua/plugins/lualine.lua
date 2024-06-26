9dcbe851a30b225ede1235aee2096b823c3a1971
Jan Lafferton <jan@lafferton.de>
2024-06-26 22:37:37 +0200

    Try some stuff

--- modules/shell/neovim/nvim/lua/plugins/lualine.lua
+++ modules/shell/neovim/nvim/lua/plugins/lualine.lua
@@ -2,6 +2,17 @@ return {
 	"lualine.nvim",
 	event = "UIEnter",
 	after = function()
-		require("lualine").setup({})
+		require("lualine").setup({
+			sections = {
+				lualine_a = {
+					{
+						"mode",
+						icons_enabled = true,
+						icon = "",
+						color = "lualine_a_normal",
+					},
+				},
+			},
+		})
 	end,
 }
