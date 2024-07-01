148f24c771caa7568f0f2eb51da97fa29951dbd7
Jan Lafferton <jan@lafferton.de>
2024-07-01 22:35:12 +0200

    Add zellij module

--- flake.nix
+++ flake.nix
@@ -107,5 +107,6 @@
       };
 
       homeManagerModules.neovim = import ./modules/shell/neovim { inherit pkgs; };
+      homeManagerModules.zellij = import ./modules/shell/zellij { inherit pkgs; };
     };
 }
