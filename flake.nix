a1c370e0d27a671079b0bf307903e351228b88b9
Jan Lafferton <jan@lafferton.de>
2024-07-01 22:48:31 +0200

    try

--- flake.nix
+++ flake.nix
@@ -107,8 +107,8 @@
       };
 
       homeManagerModules.neovim = import ./modules/shell/neovim { inherit pkgs; };
-      homeManagerModules.zellij = import ./modules/shell/zellij.nix {
+      homeManagerModules.zellij = (import ./modules/shell/zellij.nix {
         inherit inputs pkgs;
-      };
+      });
     };
 }
