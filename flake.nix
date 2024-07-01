93b9439b54d175a6db844317bb800169657fb592
Jan Lafferton <jan@lafferton.de>
2024-07-01 22:15:42 +0200

    Work

--- flake.nix
+++ flake.nix
@@ -106,6 +106,6 @@
         extraSpecialArgs = { inherit inputs outputs; };
       };
 
-      homeManagerModules.shell = import ./modules/shell { inherit inputs pkgs; };
+      homeManagerModules.neovim = import ./modules/shell/neovim { inherit pkgs; };
     };
 }
