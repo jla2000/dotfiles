fae7caac7df7294de2663acf0c7c9c52f0c99566
Jan Lafferton <jan.lafferton@vector.com>
2024-07-02 06:44:24 +0000

    Export config

---
+++ modules/shell/hm-module.nix
@@ -0,0 +1,120 @@
+{ pkgs, ... }:
+{
+  imports = [
+    ./neovim
+  ];
+
+  programs.yazi = {
+    enable = true;
+    enableBashIntegration = false;
+    enableFishIntegration = true;
+    enableNushellIntegration = true;
+  };
+
+  programs.eza = {
+    enable = true;
+    enableBashIntegration = false;
+    enableFishIntegration = true;
+    enableNushellIntegration = true;
+  };
+
+  programs.bat = {
+    enable = true;
+    #config.theme = "catppuccin";
+    #themes = {
+    #catppuccin = { src = "${inputs.catppuccin-bat-theme}/themes/Catppuccin Macchiato.tmTheme"; };
+    #};
+  };
+
+  programs.zoxide = {
+    enable = true;
+    options = [
+      "--cmd cd"
+    ];
+    enableFishIntegration = true;
+    enableBashIntegration = false;
+    enableNushellIntegration = true;
+  };
+
+  programs.starship = {
+    enable = true;
+    settings = {
+      sudo = {
+        disabled = false;
+      };
+    };
+    enableFishIntegration = true;
+    enableBashIntegration = false;
+    enableNushellIntegration = true;
+  };
+
+  programs.lazygit = {
+    enable = true;
+  };
+
+  #xdg.configFile."lazygit/config.yml".source = "${inputs.catppuccin-lazygit-theme}/themes-mergable/macchiato/sapphire.yml";
+
+  programs.fzf = {
+    enable = true;
+    enableFishIntegration = true;
+    enableBashIntegration = false;
+  };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    bashrcExtra = /* bash */ ''
+      set -o vi
+    '';
+  };
+
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = (pkgs.lib.mkOrder 0 /* fish */ ''
+      # Vim Bindings
+      fish_hybrid_key_bindings
+    '');
+
+    plugins = with pkgs.fishPlugins; [
+      { name = "bass"; src = bass.src; }
+      { name = "autopair"; src = autopair.src; }
+      { name = "puffer"; src = puffer.src; }
+      { name = "transient-fish"; src = transient-fish.src; }
+    ];
+  };
+
+  programs.nushell = {
+    enable = true;
+    configFile.text = /* nushell */ ''
+      $env.config = {
+        edit_mode: vi
+      }
+    '';
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = false;
+    enableNushellIntegration = true;
+  };
+
+  # nix = {
+  #   package = pkgs.lib.mkDefault pkgs.nix;
+  #   settings.experimental-features = [ "nix-command" "flakes" ];
+  #   registry.nixpkgs.flake = inputs.nixpkgs;
+  # };
+  #
+  #home.sessionPath = [
+  #  "~/scripts/"
+  #  "~/.local/bin"
+  #];
+
+  home.packages = with pkgs; [
+    fd
+    gdb
+    ripgrep
+    nh
+    nix-output-monitor
+    sd
+  ];
+}
