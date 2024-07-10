ce793404cc0c601ddf5ac3f33aeb6e0b9c6f28d8
Jan Lafferton <jan.lafferton@vector.com>
2024-07-10 13:23:26 +0000

    Just lazy

--- modules/neovim/nvim/lua/plugins/nvim-dap-ui.lua
+++ modules/neovim/nvim/lua/plugins/nvim-dap-ui.lua
@@ -2,20 +2,22 @@ return {
 	"nvim-dap-ui",
 	event = "DeferredUIEnter",
 	after = function()
-		local dap, dapui = require("dap"), require("dapui")
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
-
-		dapui.setup()
+		require("dapui").setup()
+		-- local dap, dapui = require("dap"), require("dapui")
+		--
+		-- dap.listeners.before.attach.dapui_config = function()
+		-- 	dapui.open()
+		-- end
+		-- dap.listeners.before.launch.dapui_config = function()
+		-- 	dapui.open()
+		-- end
+		-- dap.listeners.before.event_terminated.dapui_config = function()
+		-- 	dapui.close()
+		-- end
+		-- dap.listeners.before.event_exited.dapui_config = function()
+		-- 	dapui.close()
+		-- end
+		--
+		-- dapui.setup()
 	end,
 }
