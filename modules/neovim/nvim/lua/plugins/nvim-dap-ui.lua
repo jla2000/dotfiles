5d3030cbd80c1d70bf9590d596dff8109eb4c175
Jan Lafferton <jan.lafferton@vector.com>
2024-07-10 13:56:04 +0000

    try

--- modules/neovim/nvim/lua/plugins/nvim-dap-ui.lua
+++ modules/neovim/nvim/lua/plugins/nvim-dap-ui.lua
@@ -1,22 +1,7 @@
 return {
 	"dapui",
-	ft = "rust",
+	event = "BufEnter",
 	after = function()
-		local dap, dapui = require("dap"), require("dapui")
-
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
-
-		dapui.setup()
+		require("dapui").setup()
 	end,
 }
