41a5453bcf1a0f5b966ecfb724f4027cb058f480
Jan Lafferton <jan.lafferton@vector.com>
2024-07-15 11:00:22 +0000

    Update helix

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       flake = false;
     };
     helix = {
-      url = "github:jla2000/helix/rounded-corners";
+      url = "github:helix-editor/helix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     catppuccin-bat-theme = {
