f05060c0130d9057ed507b78cc9690dac9212e29
Jan Lafferton <jan@lafferton.de>
2024-07-28 11:35:09 +0200

    Workspace switching

--- hosts/zephyrus/home.nix
+++ hosts/zephyrus/home.nix
@@ -9,6 +9,7 @@
   home = {
     username = "jan";
     homeDirectory = "/home/jan";
+    packages = [ pkgs.dconf-editor ];
   };
 
   dconf.settings = {
@@ -28,6 +29,19 @@
       command = "firefox";
       name = "open-browser";
     };
+    "org/gnome/desktop/wm/keybindings" = {
+      close = [ "<Shift><Super>q" ];
+      switch-to-workspace-1 = [ "<Super>1" ];
+      switch-to-workspace-2 = [ "<Super>2" ];
+      switch-to-workspace-3 = [ "<Super>3" ];
+      switch-to-workspace-4 = [ "<Super>4" ];
+      move-to-workspace-1 = [ "<Shift><Super>1" ];
+      move-to-workspace-2 = [ "<Shift><Super>2" ];
+      move-to-workspace-3 = [ "<Shift><Super>3" ];
+      move-to-workspace-4 = [ "<Shift><Super>4" ];
+    };
+    "org/gnome/shell/keybindings".switch-to-application-3 = [ ];
+    "org/gnome/shell/keybindings".switch-to-application-4 = [ ];
   };
 
   home.sessionVariables.FLAKE = "${config.home.homeDirectory}/code/nixos-dotfiles";
