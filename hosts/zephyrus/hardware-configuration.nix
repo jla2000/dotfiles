f9a42d6229e0f177ccf87847f4524b3db8d1d029
Jan Lafferton <jan.lafferton@vector.com>
2024-07-29 08:38:02 +0000

    Make check pass

--- hosts/zephyrus/hardware-configuration.nix
+++ hosts/zephyrus/hardware-configuration.nix
@@ -5,7 +5,8 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" "sdhci_pci" ];
@@ -14,18 +15,19 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/3faedd21-153a-4f02-9ac6-4531a91dd805";
+    {
+      device = "/dev/disk/by-uuid/3faedd21-153a-4f02-9ac6-4531a91dd805";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/3D20-AD9D";
+    {
+      device = "/dev/disk/by-uuid/3D20-AD9D";
       fsType = "vfat";
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/014ae8e5-6052-4520-a3e8-dd19a9dcbcce"; }
-    ];
+    [{ device = "/dev/disk/by-uuid/014ae8e5-6052-4520-a3e8-dd19a9dcbcce"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
