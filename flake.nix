9072e6f0737861100f588578f663f6a6f77f7668
Jan Lafferton <jan.lafferton@vector.com>
2024-07-29 07:26:49 +0000

    Install git hooks

--- flake.nix
+++ flake.nix
@@ -1,62 +1,61 @@
 {
-  inputs =
-    {
-      nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-      nixos-hardware.url = "github:nixos/nixos-hardware";
-      nur.url = "github:nix-community/nur";
-      home-manager = {
-        url = "github:nix-community/home-manager";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
-      pre-commit-hooks = {
-        url = "github:cachix/pre-commit-hooks.nix";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
-      zellij = {
-        url = "github:zellij-org/zellij";
-        flake = false;
-      };
-      helix = {
-        url = "github:helix-editor/helix";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
-      catppuccin-bat-theme = {
-        url = "github:catppuccin/bat";
-        flake = false;
-      };
-      catppuccin-lazygit-theme = {
-        url = "github:catppuccin/lazygit";
-        flake = false;
-      };
-      huez-nvim = {
-        url = "github:vague2k/huez.nvim";
-        flake = false;
-      };
-      nerdy-nvim = {
-        url = "github:2KAbhishek/nerdy.nvim";
-        flake = false;
-      };
-      markview-nvim = {
-        url = "github:OXY2DEV/markview.nvim";
-        flake = false;
-      };
-      tid-nvim = {
-        url = "github:rachartier/tiny-inline-diagnostic.nvim";
-        flake = false;
-      };
-      nixos-wsl = {
-        url = "github:nix-community/NixOS-WSL";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
-      wgsl-analyzer = {
-        url = "github:wgsl-analyzer/wgsl-analyzer";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
-      nix-index-database = {
-        url = "github:nix-community/nix-index-database";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixos-hardware.url = "github:nixos/nixos-hardware";
+    nur.url = "github:nix-community/nur";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zellij = {
+      url = "github:zellij-org/zellij";
+      flake = false;
+    };
+    helix = {
+      url = "github:helix-editor/helix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    catppuccin-bat-theme = {
+      url = "github:catppuccin/bat";
+      flake = false;
+    };
+    catppuccin-lazygit-theme = {
+      url = "github:catppuccin/lazygit";
+      flake = false;
+    };
+    huez-nvim = {
+      url = "github:vague2k/huez.nvim";
+      flake = false;
+    };
+    nerdy-nvim = {
+      url = "github:2KAbhishek/nerdy.nvim";
+      flake = false;
+    };
+    markview-nvim = {
+      url = "github:OXY2DEV/markview.nvim";
+      flake = false;
+    };
+    tid-nvim = {
+      url = "github:rachartier/tiny-inline-diagnostic.nvim";
+      flake = false;
+    };
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    wgsl-analyzer = {
+      url = "github:wgsl-analyzer/wgsl-analyzer";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs = { self, nixpkgs, ... }@inputs:
     let
@@ -102,7 +101,7 @@
       };
     in
     {
-      checks.${system}.pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+      checks.${system}.git-check = inputs.git-hooks.lib.${system}.run {
         src = ./.;
         hooks = {
           editorconfig-checker.enable = true;
@@ -111,7 +110,9 @@
         };
       };
 
-      # TODO: Insert derivation that will insert pre commit hook
+      packages.${system}.install-git-hooks = pkgs.writeShellScriptBin "install-git-hooks" ''
+        ${self.checks.${system}.git-check.shellHook} 
+      '';
 
       nixosConfigurations = {
         "zephyrus" = nixpkgs.lib.nixosSystem {
