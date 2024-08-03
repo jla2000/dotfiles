493c88921875c734c7a92d29efd80a2d6a526cb5
Jan Lafferton <jan@lafferton.de>
2024-08-03 14:42:24 +0200

    add wallpaper

--- modules/system/stylix.nix
+++ modules/system/stylix.nix
@@ -12,10 +12,7 @@ in
   stylix = {
     enable = true;
     polarity = "dark";
-    image = pkgs.fetchurl {
-      url = "https://images.hdqwalls.com/wallpapers/linux-nixos-7q.jpg";
-      sha256 = "sha256-7rOavD/JsuvgtZpFUAVccKNY2yPvIpi8MVZ+V0EVfeQ=";
-    };
+    image = ./wallpaper.png;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
     fonts = {
       monospace = {
