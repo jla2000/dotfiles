f2f8b456d58b39bcd5fe27dfd537f0b5ba746e98
Jan Lafferton <jan@lafferton.de>
2024-08-03 16:29:44 +0200

    firefox settings

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -4,6 +4,10 @@
     enable = true;
     profiles = {
       default = {
+        settings = {
+          browser.startup.homepage = "https://google.de?q=hello";
+          browser.display.use_document_fonts = false;
+        };
         extensions = with pkgs.nur.repos.rycee.firefox-addons; [
           ublock-origin
           vimium
