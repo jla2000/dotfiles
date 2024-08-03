c049798a322f755a169f5e680516ed95c6c43708
Jan Lafferton <jan@lafferton.de>
2024-08-03 14:24:02 +0200

    fix

--- flake.nix
+++ flake.nix
@@ -15,7 +15,6 @@
       url = "github:danth/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    };
     helix = {
       url = "github:helix-editor/helix";
       inputs.nixpkgs.follows = "nixpkgs";
