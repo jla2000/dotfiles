2b31885657301e27dfdbca14e9c040a84602070e
Jan Lafferton <jan@lafferton.de>
2024-08-08 22:49:40 +0200

    Enable rust inlay hints by default

--- modules/home/neovim/nvim/lua/plugins/rustaceanvim.lua
+++ modules/home/neovim/nvim/lua/plugins/rustaceanvim.lua
@@ -5,6 +5,7 @@ vim.g.rustaceanvim = {
 			vim.keymap.set("n", "<leader>cd", "<cmd>RustLsp debuggables<cr>")
 			vim.keymap.set("n", "<leader>cle", "<cmd>RustLsp! runnables<cr>")
 			vim.keymap.set("n", "<leader>cld", "<cmd>RustLsp! debuggables<cr>")
+			vim.lsp.inlay_hint.enable()
 		end,
 		default_settings = {
 			["rust-analyzer"] = {
