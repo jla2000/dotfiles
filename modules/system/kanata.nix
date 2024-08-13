b985c47bd26a681313508917609d0f4ef94c2437
Jan Lafferton <jan@lafferton.de>
2024-08-13 19:33:23 +0200

    fix kanata

--- modules/system/kanata.nix
+++ modules/system/kanata.nix
@@ -1,9 +1,9 @@
 { pkgs, lib, ... }:
 let
   kanata-config = pkgs.writeText "config.kbd" ''
-    (defsrc caps lctrl esc)
-    (defalias escctrl (tap-hold 140 140 esc lctrl))
-    (deflayer base @escctrl nop0 nop0)
+    (defsrc caps)
+    (defalias escctrl (tap-hold 150 150 esc lctrl))
+    (deflayer base @escctrl)
   '';
 in
 {
