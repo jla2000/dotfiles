ed098907651aefeac5eda9cf0fc6c896617e4f0b
Jan Lafferton <jan.lafferton@vector.com>
2024-08-26 13:07:37 +0000

    snippet to debug plugin loading

--- modules/home/neovim/nvim/init.lua
+++ modules/home/neovim/nvim/init.lua
@@ -1,6 +1,14 @@
 require("options")
 require("autocmds")
 require("keymaps")
+
+-- vim.g.lz_n = {
+-- 	load = function(name)
+-- 		vim.notify("Loading plugin: " .. name)
+-- 		vim.cmd.packadd(name)
+-- 	end,
+-- }
+
 require("lz.n").load("plugins")
 
 vim.cmd.colorscheme("tokyodark")
