6ec1c34f040a1f5ee73b3b7e59b4e5024422dd0c
Jan Lafferton <jan.lafferton@vector.com>
2024-08-28 09:09:32 +0000

    Update

--- modules/home/neovim/nvim/init.lua
+++ modules/home/neovim/nvim/init.lua
@@ -2,12 +2,12 @@ require("options")
 require("autocmds")
 require("keymaps")
 
--- vim.g.lz_n = {
--- 	load = function(name)
--- 		vim.notify("Loading plugin: " .. name)
--- 		vim.cmd.packadd(name)
--- 	end,
--- }
+vim.g.lz_n = {
+	load = function(name)
+		vim.notify("Loading plugin: " .. name)
+		vim.cmd.packadd(name)
+	end,
+}
 
 require("lz.n").load("plugins")
 
