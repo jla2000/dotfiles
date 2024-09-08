9907735d53f8d2913181d21a816d940cae2f6094
Jan Lafferton <jan@lafferton.de>
2024-09-08 11:02:43 +0200

    indent

--- modules/home/neovim/nvim/lua/plugins/mason.lua
+++ modules/home/neovim/nvim/lua/plugins/mason.lua
@@ -1,5 +1,5 @@
 return {
-	{ "williamboman/mason-lspconfig.nvim", enabled = false },
-	{ "williamboman/mason.nvim", enabled = false },
-	{ "jaybaby/mason-nvim-dap.nvim", enabled = false },
+  { "williamboman/mason-lspconfig.nvim", enabled = false },
+  { "williamboman/mason.nvim", enabled = false },
+  { "jaybaby/mason-nvim-dap.nvim", enabled = false },
 }
