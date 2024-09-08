eb04bf38d993c70016adedfc9f8ac9c189870ef8
Jan Lafferton <jan@lafferton.de>
2024-09-08 11:11:31 +0200

    add some color schemes

--- modules/home/neovim/nvim/lua/plugins/colorscheme.lua
+++ modules/home/neovim/nvim/lua/plugins/colorscheme.lua
@@ -4,4 +4,10 @@ return {
     opts = { colorscheme = "onedark_dark" },
   },
   { "olimorris/onedarkpro.nvim", opts = {} },
+  { "tiagovla/tokyodark.nvim", opts = {} },
+  { "2giosangmitom/nightfall.nvim", opts = {} },
+  { "slugbyte/lackluster.nvim", opts = {} },
+  { "niyabits/calvera-dark.nvim", opts = {} },
+  { "0xstepit/flow.nvim", opts = {} },
+  { "catppuccin/nvim", opts = {} },
 }
