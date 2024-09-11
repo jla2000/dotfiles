207b8dca9416629d048ee00a9d20337c15171096
Jan Lafferton <jan.lafferton@vector.com>
2024-09-11 07:53:36 +0000

    Markdown stuff

--- modules/home/neovim/nvim/lua/plugins/markdown.lua
+++ modules/home/neovim/nvim/lua/plugins/markdown.lua
@@ -1,8 +1,20 @@
 return {
   {
     "OXY2DEV/markview.nvim",
-    ft = "markdown",
-    opts = {},
+    lazy = false,
+    opts = {
+      modes = { "n", "no", "c" }, -- Change these modes to what you need
+
+      hybrid_modes = { "n" }, -- Uses this feature on normal mode
+
+      -- This is nice to have
+      callbacks = {
+        on_enable = function(_, win)
+          vim.wo[win].conceallevel = 2
+          vim.wo[win].concealcursor = "c"
+        end,
+      },
+    },
   },
   { "iamcco/markdown-preview.nvim", enabled = false },
   { "MeanderingProgrammer/markdown.nvim", enabled = false },
