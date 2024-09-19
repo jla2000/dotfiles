f2d67172ea1f90c156f0f657b6cad7f3f1a912f0
Jan Lafferton <jan.lafferton@vector.com>
2024-09-19 06:36:30 +0000

    fix markdown + update plugins

--- modules/home/neovim/config/lua/plugins/markdown.lua
+++ modules/home/neovim/config/lua/plugins/markdown.lua
@@ -18,4 +18,5 @@ return {
   },
   { "iamcco/markdown-preview.nvim", enabled = false },
   { "MeanderingProgrammer/markdown.nvim", enabled = false },
+  { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
 }
