c624be3ca327624c0637546d77ea0a4acf238896
Jan Lafferton <jan.lafferton@vector.com>
2025-01-08 11:21:32 +0000

    make alacritty pretty

--- modules/home/alacritty.nix
+++ modules/home/alacritty.nix
@@ -7,12 +7,22 @@
         size = lib.mkDefault 16;
         normal = {
           family = lib.mkDefault "JetBrainsMono NF";
-          style = lib.mkForce "Medium";
+          style = lib.mkForce "Bold";
         };
         italic = {
           family = lib.mkDefault "JetBrainsMono NF";
-          style = lib.mkForce "Medium Italic";
+          style = lib.mkForce "Bold Italic";
         };
+        bold = {
+          family = lib.mkDefault "JetBrainsMono NF";
+          style = lib.mkForce "ExtraBold";
+        };
+        bold_italic = {
+          family = lib.mkDefault "JetBrainsMono NF";
+          style = lib.mkForce "ExtraBold Italic";
+        };
+        builtin_box_drawing = false;
+        offset.x = 1;
       };
 
       window = {
