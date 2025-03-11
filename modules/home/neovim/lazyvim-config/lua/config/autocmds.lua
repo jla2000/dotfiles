d88c512d8f217bce747cfd6f915b03049df84206
Jan Lafferton <jan.lafferton@vector.com>
2025-03-11 15:02:44 +0000

    Save folds

--- modules/home/neovim/lazyvim-config/lua/config/autocmds.lua
+++ modules/home/neovim/lazyvim-config/lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("BufWinLeave", {
+  callback = function()
+    vim.cmd([[ mkview ]])
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  callback = function()
+    vim.cmd([[ loadview ]])
+  end,
+})
