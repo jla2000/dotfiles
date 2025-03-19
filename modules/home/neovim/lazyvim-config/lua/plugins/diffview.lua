6016c554e1967f9ae3631572efa107105858d9da
Jan Lafferton <jan.lafferton@vector.com>
2025-03-19 08:12:07 +0000

    Diffview shortcut

--- modules/home/neovim/lazyvim-config/lua/plugins/diffview.lua
+++ modules/home/neovim/lazyvim-config/lua/plugins/diffview.lua
@@ -2,4 +2,16 @@ return {
   "sindrets/diffview.nvim",
   cmd = "DiffviewOpen",
   opts = {},
+  keys = {
+    {
+      "<leader>v",
+      function()
+        if next(require("diffview.lib").views) == nil then
+          vim.cmd("DiffviewOpen")
+        else
+          vim.cmd("DiffviewClose")
+        end
+      end,
+    },
+  },
 }
