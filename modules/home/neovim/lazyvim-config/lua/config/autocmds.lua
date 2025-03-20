a2c8a29923f31ebddbb2aaca3244ea88cbc1a418
Jan Lafferton <jan.lafferton@vector.com>
2025-03-20 11:38:49 +0100

    Update neovim

--- modules/home/neovim/lazyvim-config/lua/config/autocmds.lua
+++ modules/home/neovim/lazyvim-config/lua/config/autocmds.lua
@@ -2,11 +2,11 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "rust",
-  callback = function()
-    require("vim.treesitter.query").set("rust", "folds", "((line_comment)+ @fold)")
-    vim.opt_local.foldlevel = 0
-    vim.opt_local.foldtext = "foldtext()"
-  end,
-})
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "rust",
+--   callback = function()
+--     require("vim.treesitter.query").set("rust", "folds", "((line_comment)+ @fold)")
+--     vim.opt_local.foldlevel = 0
+--     vim.opt_local.foldtext = "foldtext()"
+--   end,
+-- })
