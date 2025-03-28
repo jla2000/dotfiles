bbb414d2c2ad22e4b0510b1a86aa7fbdc1da5fb3
Jan Lafferton <jan.lafferton@vector.com>
2025-03-28 09:41:50 +0100

    Fix helix shaise

--- flake.nix
+++ flake.nix
@@ -75,9 +75,4 @@
         };
       };
     };
-
-  nixConfig = {
-    extra-substituters = [ "https://helix.cachix.org" ];
-    extra-trusted-public-keys = [ "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs=" ];
-  };
 }
