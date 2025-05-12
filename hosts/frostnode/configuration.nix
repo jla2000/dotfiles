caf3bef49269974528b105ec29010fd79aa3fcb9
Jan Lafferton <jan@lafferton.de>
2025-05-12 14:02:09 +0200

    fix frostnode

--- hosts/frostnode/configuration.nix
+++ hosts/frostnode/configuration.nix
@@ -33,7 +33,6 @@
   home-manager.backupFileExtension = "bak";
 
   programs.nix-index-database.comma.enable = true;
-  stylix.fonts.sizes.terminal = 16;
   stylix.fonts.monospace.name = "Iosevka Nerd Font";
 
   system.stateVersion = "24.05";
