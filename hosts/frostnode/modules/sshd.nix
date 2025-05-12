8d91aa82e65254eaf5e81503ae294faec51c8794
Jan Lafferton <jan@lafferton.de>
2025-05-12 11:04:46 +0000

    Add key

--- hosts/frostnode/modules/sshd.nix
+++ hosts/frostnode/modules/sshd.nix
@@ -5,5 +5,6 @@
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGNnFCewQPPP4nPlJqxa0Zj0TGIQwiudOR2D0qtZ115J jan@nixos"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDbi4nQxiQN/2HFX7mx0GL1TsNbfHFuZXfDyuN1/noIC jlafferton2nixos"
   ];
 }
