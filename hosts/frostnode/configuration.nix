da76348013f72d467bff0715b34d2dc2708c4cc7
Jan Lafferton <jan.lafferton@vector.com>
2025-05-14 11:32:21 +0200

    Disable password authentication

--- hosts/frostnode/configuration.nix
+++ hosts/frostnode/configuration.nix
@@ -35,7 +35,10 @@
   stylix.fonts.monospace.name = "Iosevka Nerd Font";
   services.getty.autologinUser = "root";
 
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = false;
+  };
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDbi4nQxiQN/2HFX7mx0GL1TsNbfHFuZXfDyuN1/noIC jlafferton@nixos"
   ];
