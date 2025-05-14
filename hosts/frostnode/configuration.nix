d022705eec0f039e0c6e78f8719af00dd40f3efb
Jan Lafferton <jan.lafferton@vector.com>
2025-05-14 11:36:57 +0200

    Add windows key

--- hosts/frostnode/configuration.nix
+++ hosts/frostnode/configuration.nix
@@ -40,7 +40,10 @@
     settings.PasswordAuthentication = false;
   };
   users.users.root.openssh.authorizedKeys.keys = [
+    # NixOS-Unstable
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDbi4nQxiQN/2HFX7mx0GL1TsNbfHFuZXfDyuN1/noIC jlafferton@nixos"
+    # Windows
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOlcwpZgZdSdMcLua9Tf6y6kMQgwMkTTRDhcAR8FSwOj vector\\jlafferton@DE18314NB"
   ];
 
   system.stateVersion = "24.05";
