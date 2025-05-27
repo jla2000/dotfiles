088757acda2d369f02d748504a33af444b3aa4c4
Jan Lafferton <jan.lafferton@vector.com>
2025-05-27 14:04:16 +0200

    Try weird stuff

--- hosts/frostnode/configuration.nix
+++ hosts/frostnode/configuration.nix
@@ -17,6 +17,14 @@
   boot.loader.efi.canTouchEfiVariables = true;
   services.getty.autologinUser = "root";
 
+  networking.nftables.enable = true;
+  networking.firewall.enable = true;
+  networking.firewall.extraCommands = ''
+    ip link set dev eth0 mtu 1400
+    iptables -t mangle -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
+    iptables -t mangle -A OUTPUT -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
+  '';
+
   system.stateVersion = "24.05";
 }
 
