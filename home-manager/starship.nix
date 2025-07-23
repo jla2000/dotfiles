44c01db149fad04b2c5bec8aae69f683dab79711
Jan Lafferton <jan.lafferton@vector.com>
2025-07-23 10:15:23 +0200

    Disable starship-jj

--- home-manager/starship.nix
+++ home-manager/starship.nix
@@ -3,20 +3,20 @@
   programs.starship = {
     enable = true;
     settings = {
-      format = ''
-        $directory$custom$all
-      '';
+      #format = ''
+      #  $directory$custom$all
+      #'';
       sudo.disabled = true;
-      custom.jj = {
-        command = "prompt";
-        format = "$output";
-        ignore_timeout = true;
-        shell = [ "${pkgs.starship-jj}/bin/starship-jj" "--ignore-working-copy" "starship" ];
-        use_stdin = false;
-        when = true;
-      };
-      git_commit.disabled = true;
-      git_branch.disabled = true;
+      #custom.jj = {
+      #  command = "prompt";
+      #  format = "$output";
+      #  ignore_timeout = true;
+      #  shell = [ "${pkgs.starship-jj}/bin/starship-jj" "--ignore-working-copy" "starship" ];
+      #  use_stdin = false;
+      #  when = true;
+      #};
+      #git_commit.disabled = true;
+      #git_branch.disabled = true;
     };
     enableFishIntegration = true;
     enableBashIntegration = false;
