e19c51cd10c694e7d0df811778704b6db5db127b
Jan Lafferton <jan.lafferton@vector.com>
2025-08-15 10:11:50 +0200

    Tmux fixes

--- home-manager/tmux.nix
+++ home-manager/tmux.nix
@@ -6,16 +6,14 @@
     package = pkgs.tmux;
     prefix = "C-s";
     enable = true;
-    newSession = true;
     mouse = true;
     keyMode = "vi";
     escapeTime = 10;
     terminal = "tmux-256color";
+    shell = "${pkgs.fish}/bin/fish";
+    focusEvents = true;
     extraConfig = /* tmux */ ''
       set -ag terminal-overrides ",xterm-256color:RGB"
-
-      # Fixes navigation between tmux and nvim
-      set -g focus-events on
 
       unbind r
       bind-key r source-file ~/.config/tmux/tmux.conf
@@ -23,8 +21,6 @@
       bind-key -T copy-mode-vi v send-keys -X begin-selection
       bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
       bind-key -T copy-mode-vi i send-keys -X cancel
-
-      set -g default-shell ${lib.getExe pkgs.fish}
 
       # Image nvim
       set -g allow-passthrough on
@@ -35,9 +31,7 @@
       bind % split-window -h -c "#{pane_current_path}"
 
       bind -n C-g new-window -c "#{pane_current_path}" -n jjui jjui
-
-      # Tmux attach behavior
-      new-session -n $HOST
+      bind -n C-h new-window -c "#{pane_current_path}" -n bacon bacon
 
       # Border style
       set -g popup-border-lines rounded
