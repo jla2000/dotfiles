beed6fa3e23bc7a0ea54b76a8390fc09bcc6485b
Jan Lafferton <jan@lafferton.de>
2025-12-30 17:17:02 +0100

    mongo db

--- systems/x86_64-linux/frostnode/modules/ai.nix
+++ systems/x86_64-linux/frostnode/modules/ai.nix
@@ -2,10 +2,10 @@
 
 {
   services.ollama.enable = true;
-  services.mongodb.enable = true;
-
-  services.n8n = {
+  services.n8n.enable = true;
+  services.mongodb = {
     enable = true;
+    package = pkgs.mongodb-ce;
   };
 
   services.caddy = {
@@ -13,12 +13,6 @@
     virtualHosts = {
       "n8n.frostnode.de".extraConfig = ''
         reverse_proxy http://127.0.0.1:5678
-      '';
-      "db.frostnode.de".extraConfig = ''
-        basicauth {
-          admin $2a$14$nLOHPI5MQP5iiX8vfOUZX.GczR2uTYeHpDiZ.Otp4HOZOjh23tuJu
-        }
-        reverse_proxy http://127.0.0.1:6333
       '';
     };
   };
