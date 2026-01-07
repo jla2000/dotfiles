03fd43145bb2539e11d14d11c918ad63e208aa22
Lafferton, Jan <jan.lafferton@vector.com>
2026-01-07 14:01:45 +0100

    Fix ghostty shader

--- modules/home/shell/ghostty.nix
+++ modules/home/shell/ghostty.nix
@@ -1,10 +1,4 @@
 { pkgs, lib, ... }:
-let
-  shader = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/KroneCorylus/ghostty-shader-playground/refs/heads/main/public/shaders/cursor_smear.glsl";
-    hash = "sha256-+5jUoSYIv3YJ/1ge7Bj49+ZVtz890cYvUng33UgGakM=";
-  };
-in
 {
   programs.ghostty = {
     enable = true;
@@ -16,7 +10,7 @@ in
       window-width = 135;
       window-height = 34;
       confirm-close-surface = "false";
-      custom-shader = "${shader}";
+      custom-shader = ./cursor_smear.glsl;
     };
   };
 }
