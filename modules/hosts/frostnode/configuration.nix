8e163225248c8532d3e8c8c0412152b87d1fcdf7
Jan Lafferton <jan@lafferton.de>
2026-03-27 17:26:11 +0100

    Try to fix

--- modules/hosts/frostnode/configuration.nix
+++ modules/hosts/frostnode/configuration.nix
@@ -1,7 +1,7 @@
 { self, ... }:
 {
   flake.nixosModules.frostnode =
-    { ... }:
+    { pkgs, lib, ... }:
     {
       imports = [
         self.nixosModules.common
@@ -21,16 +21,18 @@
           X11Forwarding = true;
         };
       };
-      users.users.root.openssh.authorizedKeys.keys = [
-        # Zephyrus
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGblR6muDT3qYK2I/B1ba+Z1XbTe4yM3S1jtJgUVGEbu jan@nixos"
-        # NixOS-Unstable
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDbi4nQxiQN/2HFX7mx0GL1TsNbfHFuZXfDyuN1/noIC jlafferton@nixos"
-        # Windows
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOlcwpZgZdSdMcLua9Tf6y6kMQgwMkTTRDhcAR8FSwOj vector\\jlafferton@DE18314NB"
-        # Local root
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINT2Uf7Nxm8XzIeE2P1ZWO4X9UPfi5ad2gSfQQYwA8/+ root@nixos"
-      ];
+      users.users.root = {
+        openssh.authorizedKeys.keys = [
+          # Zephyrus
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGblR6muDT3qYK2I/B1ba+Z1XbTe4yM3S1jtJgUVGEbu jan@nixos"
+          # NixOS-Unstable
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDbi4nQxiQN/2HFX7mx0GL1TsNbfHFuZXfDyuN1/noIC jlafferton@nixos"
+          # Windows
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOlcwpZgZdSdMcLua9Tf6y6kMQgwMkTTRDhcAR8FSwOj vector\\jlafferton@DE18314NB"
+          # Local root
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINT2Uf7Nxm8XzIeE2P1ZWO4X9UPfi5ad2gSfQQYwA8/+ root@nixos"
+        ];
+      };
 
       swapDevices = [
         {
