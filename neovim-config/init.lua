a4b83e76b36b93ae9fdb63dc42d7cd2cd9d3b2af
Jan Lafferton <jan@lafferton.de>
2026-04-28 10:04:41 +0200

    Better format autocmd

--- neovim-config/init.lua
+++ neovim-config/init.lua
@@ -91,13 +91,15 @@ vim.api.nvim_create_autocmd("LspAttach", {
     if client:supports_method("textDocument/inlineCompletion") then
       vim.lsp.inline_completion.enable(true, { bufnr = args.buf })
     end
+    if client:supports_method("textDocument/formatting") then
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        buffer = args.buf,
+        callback = function()
+          vim.lsp.buf.format({ async = false, bufnr = args.buf })
+        end
+      })
+    end
   end,
-})
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-  callback = function()
-    vim.lsp.buf.format({ async = false })
-  end
 })
 
 vim.api.nvim_create_autocmd("FileType", {
